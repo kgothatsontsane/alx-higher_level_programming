c4fcecb5a49b0ab510ef88aa799cb748cd140f4b
Kgothatso <nvisionfactory@gmail.com>
2024-06-20 07:20:53 -0700

    edit task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""returns True if the object is exactly an instance of the specified class
-; otherwise False.
+"""Module checks class and subclass
 """
 
 
-def is_same_class(obj, a_class):
-    """returns True if the object is exactly an instance"""
-    return True if type(obj) == a_class else False
+def is_kind_of_class(obj, a_class):
+    """
+    returns True if the object is an instance of, or if the object
+    is an instance
+    of a class that inherited from, the specified class ; otherwise False
+    """
+    return isinstance(obj, a_class)
