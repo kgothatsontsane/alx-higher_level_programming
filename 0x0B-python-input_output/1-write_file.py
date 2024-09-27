7920a063f4a8ba6d1135a138bab17acadc6fada4
Kgothatso Ntsane <nvisionfactory@gmail.com>
2024-09-27 16:09:05 +0000

    Refactor write_file function to include detailed docstring and shebang

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This function writes a string to a text file (UTF8) and returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns the number of characters written.
+
+    This function opens a file in write mode with UTF-8 encoding,
+    writes the provided text to the file, and returns the number of characters written.
+    If the file does not exist, it will be created. If the file already exists,
+    its content will be overwritten.
+
+    Args:
+        filename (str): The path to the file to be written to.
+        text (str): The text to write to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
+
+    with open(filename, "w", encoding='utf-8') as f:
+        return f.write(text)
