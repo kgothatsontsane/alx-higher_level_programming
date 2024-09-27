75a1942e1920869df1a9d29a55cbb180ada6aa20
Kgothatso Ntsane <nvisionfactory@gmail.com>
2024-09-27 15:57:33 +0000

    Refactor read_file function to include detailed docstring

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,16 +3,21 @@
 This function reads a text file (UTF8) and prints it to stdout
 """
 
+
 def read_file(filename=""):
     """
-    Reads the content of a specified file and prints it to the standard output.
+    Reads the content of a file and prints it to the standard output.
 
-    This function opens a file in read mode with UTF-8 encoding, reads its content, 
-    and prints it without adding an extra newline at the end. If the file does not exist 
-    or cannot be opened, an error will be raised.
+    This function opens a file in read mode with UTF-8 encoding,
+    reads its content, and prints it without adding an
+    extra newline at the end.
+
+    If the file does not exist or cannot be opened,
+    an error will be raised.
 
     Args:
-        filename (str): The path to the file to be read. Defaults to an empty string.
+        filename (str): The path to the file to be read.
+        Defaults to an empty string.
 
     Returns:
         None
