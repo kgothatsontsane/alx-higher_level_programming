7420111034ced6a1a08ee86cfa64ef75054d61f5
Kgothatso Ntsane <nvisionfactory@gmail.com>
2024-09-29 10:47:06 +0000

    reate append_write function

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This function writes a string to a text file (UTF8)
+and returns the number of characters written.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns
+    the number of characters written.
+
+    This function opens a file in write mode with UTF-8 encoding,
+    writes the provided text to the file, and returns
+    the number of characters written. If the file does not exist,
+    it will be created. If the file already exists,
+    its content will be overwritten.
+
+    Args:
+        filename (str): The path to the file to be written to.
+        text (str): The text to write to the file.
+
+    Returns:
+        int: The number of characters written to the file.
+    """
+
+    with open(filename, "a", encoding='utf-8') as file:
+        return file.write(text)
+
